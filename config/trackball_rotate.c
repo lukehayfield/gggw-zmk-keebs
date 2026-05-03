718ec63b262ebec5afb7c817d0c735e73e567f05
lukehayfield <112131823+lukehayfield@users.noreply.github.com>
2026-05-03 20:40:14 +0100

    Refactor trackball rotation logic in trackball_rotate.c

--- config/trackball_rotate.c
+++ config/trackball_rotate.c
@@ -1,6 +1,5 @@
 #include <zephyr/device.h>
 #include <zmk/input_processor.h>
-#include <zmk/split/bluetooth/central.h>
 
 #define ROTATE 0.087f  // ~5 degrees
 
@@ -13,15 +12,8 @@ static int trackball_rotate_process(struct zmk_input_processor *proc,
     float x = evt->rel.x;
     float y = evt->rel.y;
 
-    bool is_central = IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL);
-
-    float dir = is_central ? -1.0f : 1.0f;
-
-    float new_x = x - (y * ROTATE * dir);
-    float new_y = y + (x * ROTATE * dir);
-
-    evt->rel.x = new_x;
-    evt->rel.y = new_y;
+    evt->rel.x = x - (y * ROTATE);
+    evt->rel.y = y + (x * ROTATE);
 
     return 0;
 }
